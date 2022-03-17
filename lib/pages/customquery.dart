4e0141f5e136ab047935f5ed500bcaee77843169
ShubhamDalvi1999 <dshubhamp1999@gmail.com>
2022-03-18 01:56:05 +0530

    modifying fields

--- lib/pages/customquery.dart
+++ lib/pages/customquery.dart
@@ -14,6 +14,7 @@ class _CustomQueryState extends State<CustomQuery> {
   final age = TextEditingController();
   final rentGiven = TextEditingController();
   List<Map<String, dynamic>> listmap = [];
+  final _formKey = GlobalKey<FormState>(); //Formkey to validate entered values
 
   Future<List<Map<String, dynamic>>> customquery() async {
     listmap = (await dbhelper.queryspecific(age.text, rentGiven.text))!;
@@ -26,77 +27,109 @@ class _CustomQueryState extends State<CustomQuery> {
 
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: age,
-            decoration: const InputDecoration(
-              hintText: 'enter which age customers do u want to see',
-              labelText: 'Age',
-              prefixIcon: Icon(Icons.graphic_eq_rounded),
-              border: OutlineInputBorder(),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextField(
-            controller: rentGiven,
-            decoration: const InputDecoration(
-              hintText:
-                  'Enter 0 if u want to see customers who have given rent else enter 1',
-              labelText: 'Monthly rent given or not (0/1)',
-              prefixIcon: Icon(Icons.monetization_on_outlined),
-              border: OutlineInputBorder(),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            width: _mediaQuery.width * 0.89,
-            height: _mediaQuery.height * 0.4,
-            child: Expanded(
-              child: Scrollbar(
-                showTrackOnHover: true,
-                child: ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemCount: listmap.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        leading: const Icon(Icons.person),
-                        title:
-                            Text(listmap[index].values.elementAt(2).toString()),
-                        subtitle: Text(
-                            'Monthly Rent: ${listmap[index].values.elementAt(3).toString()}'),
-                      );
-                    }),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: age,
+              validator: (value) {
+                if (value == null) {
+                  return "Age cannot be blank";
+                } else if (int.tryParse(value) != null) {
+                  //if stirng is only number
+                  if (int.parse(value) <= 0) {
+                    return "value cannot be 0 or negative";
+                  }
+                } else if (int.tryParse(value) == null) {
+                  return "enter a number";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                hintText: 'enter which age customers do u want to see',
+                labelText: 'Age',
+                prefixIcon: Icon(Icons.graphic_eq_rounded),
+                border: OutlineInputBorder(),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                customquery();
-              });
-            },
-            style: ElevatedButton.styleFrom(
-              onPrimary: Colors.white,
-              primary: Colors.purple,
-              onSurface: Colors.grey,
-              side: const BorderSide(color: Colors.black, width: 1),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              elevation: 20,
+            const SizedBox(
+              height: 20,
             ),
-            child: const Text("Show results"),
-          ),
-        ],
+            TextFormField(
+              controller: rentGiven,
+              validator: (value) {
+                if (value == null) {
+                  return "Cannot be blank";
+                } else if (int.tryParse(value) != null) {
+                  if (int.parse(value) > 1) {
+                    return "It cannot be other than 0 or 1";
+                  } else if (int.parse(value) < 0) {
+                    return "It cannot be other than 0 or 1";
+                  }
+                } else if (int.tryParse(value) == null) {
+                  return "enter a number only";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                hintText:
+                    'Enter 0 if u want to see customers who have given rent else enter 1',
+                labelText: 'Monthly rent given or not (0/1)',
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: _mediaQuery.width * 0.89,
+              height: _mediaQuery.height * 0.4,
+              child: Expanded(
+                child: Scrollbar(
+                  showTrackOnHover: true,
+                  child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: listmap.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: const Icon(Icons.person),
+                          title: Text(
+                              listmap[index].values.elementAt(2).toString()),
+                          subtitle: Text(
+                              'Monthly Rent: ${listmap[index].values.elementAt(3).toString()}'),
+                        );
+                      }),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  setState(() {
+                    customquery();
+                  });
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                onPrimary: Colors.white,
+                primary: Colors.purple,
+                onSurface: Colors.grey,
+                side: const BorderSide(color: Colors.black, width: 1),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                elevation: 20,
+              ),
+              child: const Text("Show results"),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/insert.dart
+++ lib/pages/insert.dart
@@ -1,6 +1,7 @@
 import 'package:database_curd_demo_app/models/index.dart';
 import 'package:flutter/material.dart';
-
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
 import '../FireStoreDatabase/UserData.dart';
 import '../models/dbhelper.dart';
 
@@ -9,6 +10,26 @@ class Insertdata extends StatefulWidget {
 
   @override
   State<Insertdata> createState() => _InsertdataState();
+}
+
+class BasicTimeField extends StatelessWidget {
+  final format = DateFormat("hh:mm a");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('Basic time field (${format.pattern})'),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) async {
+          final TimeOfDay? time = await showTimePicker(
+            context: context,
+            initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+          );
+          return time == null ? null : DateTimeField.convert(time);
+        },
+      ),
+    ]);
+  }
 }
 
 class _InsertdataState extends State<Insertdata> {
@@ -22,6 +43,8 @@ class _InsertdataState extends State<Insertdata> {
   final partialrentgivenondate = TextEditingController();
   int rentgiven = 100;
   int rentradiogroup = 10;
+  final _formKey = GlobalKey<FormState>();
+  final format = DateFormat("yyyy-MM-dd HH:mm");
 
   final dbhelper = DatabaseHelper.instance;
 
@@ -61,118 +84,145 @@ class _InsertdataState extends State<Insertdata> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(18.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: personName,
-              decoration: const InputDecoration(
-                hintText: 'enter a rented persons name',
-                labelText: 'Person name ',
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: age,
-              decoration: const InputDecoration(
-                hintText: 'enter ur age',
-                labelText: 'Age',
-                prefixIcon: Icon(Icons.graphic_eq_rounded),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: monthlyrent,
-              decoration: const InputDecoration(
-                hintText: 'enter his monthly rent',
-                labelText: 'Monthly rent',
-                prefixIcon: Icon(Icons.monetization_on_outlined),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: contactno,
-              decoration: const InputDecoration(
-                hintText: 'enter Contact Number',
-                labelText: 'Contact Number',
-                prefixIcon: Icon(Icons.monetization_on_outlined),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: trust,
-              decoration: const InputDecoration(
-                hintText: 'What is the trust rating?',
-                labelText: 'Trust Rating',
-                prefixIcon: Icon(Icons.monetization_on_outlined),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    Radio(
-                        value: 1,
-                        groupValue: rentradiogroup,
-                        onChanged: (val) {
-                          setState(() {
-                            rentradiogroup = 1;
-                            rentgiven = 1;
-                          });
-                        }),
-                    const Text("True"),
-                  ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: personName,
+                decoration: const InputDecoration(
+                  hintText: 'enter a rented persons name',
+                  labelText: 'Person name ',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(),
                 ),
-                Row(
-                  children: [
-                    Radio(
-                        value: 0,
-                        groupValue: rentradiogroup,
-                        onChanged: (val) {
-                          setState(() {
-                            rentradiogroup = 0;
-                            rentgiven = 0;
-                          });
-                        }),
-                    const Text("False"),
-                  ],
-                )
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                insertdata();
-              },
-              style: ElevatedButton.styleFrom(
-                onPrimary: Colors.white,
-                primary: Colors.purple,
-                onSurface: Colors.grey,
-                side: const BorderSide(color: Colors.black, width: 1),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                elevation: 20,
               ),
-              child: const Text("Add data to database"),
-            ),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: age,
+                decoration: const InputDecoration(
+                  hintText: 'enter ur age',
+                  labelText: 'Age',
+                  prefixIcon: Icon(Icons.graphic_eq_rounded),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: monthlyrent,
+                decoration: const InputDecoration(
+                  hintText: 'enter his monthly rent',
+                  labelText: 'Monthly rent',
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: contactno,
+                decoration: const InputDecoration(
+                  hintText: 'enter Contact Number',
+                  labelText: 'Contact Number',
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: trust,
+                decoration: const InputDecoration(
+                  hintText: 'What is the trust rating?',
+                  labelText: 'Trust Rating',
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              DateTimeField(
+                format: format,
+                onShowPicker: (context, currentValue) async {
+                  final date = await showDatePicker(
+                      context: context,
+                      firstDate: DateTime(1900),
+                      initialDate: currentValue ?? DateTime.now(),
+                      lastDate: DateTime(2100));
+                  if (date != null) {
+                    final time = await showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.fromDateTime(
+                          currentValue ?? DateTime.now()),
+                    );
+                    return DateTimeField.combine(date, time);
+                  } else {
+                    return currentValue;
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const Text("Full Rent Staus: "),
+                      Radio(
+                          value: 1,
+                          groupValue: rentradiogroup,
+                          onChanged: (val) {
+                            setState(() {
+                              rentradiogroup = 1;
+                              rentgiven = 1;
+                            });
+                          }),
+                      const Text("True"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Radio(
+                          value: 0,
+                          groupValue: rentradiogroup,
+                          onChanged: (val) {
+                            setState(() {
+                              rentradiogroup = 0;
+                              rentgiven = 0;
+                            });
+                          }),
+                      const Text("False"),
+                    ],
+                  )
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  insertdata();
+                },
+                style: ElevatedButton.styleFrom(
+                  onPrimary: Colors.white,
+                  primary: Colors.purple,
+                  onSurface: Colors.grey,
+                  side: const BorderSide(color: Colors.black, width: 1),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  elevation: 20,
+                ),
+                child: const Text("Add data to database"),
+              ),
+            ],
+          ),
         ),
       ),
     );
